a2fe408d521e92a13db8727a76c4f6c99c36a459
Naren Yenuganti <yrnaren1@gmail.com>
2021-05-15 17:44:35 -0700

    added click with air sensor

--- software/M0_cursor_control/M0_cursor_control.ino
+++ software/M0_cursor_control/M0_cursor_control.ino
@@ -1,69 +1,174 @@
-// Cursor movement using Trinket M0
+/*
+  Description: Adaptive hands-free computer mouse that can be used by people with quadriplegia. 
+  Supports left click, right click, scroll, and zoom on the hardware-level.
+  Date: May 2021
+  Authors: Naren Yenuganti and Dylan Chow      
+*/
 
 #include <Mouse.h>
+#include <Keyboard.h>
+#include <ClickButton.h>
 
+// Pins
 int x = A4;
 int y = A3;
-int airSensor = A0;
-const int sensitivity = 23; // larger sensitivity --> slower mouse
-const unsigned long loopPeriod = 15; // larger loop_period --> more delay
-int yZero, xZero;
-boolean debugCursor = false;
-boolean debugAirSensor = true;
+int air_sensor = A0;
+int joystick_sel_pin = 7;
 
+// Mouse Movement Controls
+const int sensitivity = 23; // larger sensitivity --> slower mouse
+const unsigned long loop_period = 15; // larger loop_period --> more delay
+int y_zero, x_zero;
+boolean invert = true;
+
+// Click Flags
+int right_click_flag = 0;
+int left_click_flag = 0;
+int scroll_flag = 0;
+int zoom_flag = 0;
+
+// Debug Constants
+boolean debug_cursor = false;
+boolean debug_air_sensor = false;
+
+// Multi Click Constants
+int debounce_time = 20;   // Debounce timer in ms
+int multi_click_time = 250;  // Time limit for multi clicks
+int long_click_time = 250;  // time until "held-down clicks" register
+
+// Air Sensor Constants
+int rest = 361;
+int max_sip = 13;
+int max_puff = 648;
+
+// Double Click Detection 
+ClickButton key(joystick_sel_pin, LOW, CLICKBTN_PULLUP);
 
 void setup() {
   pinMode(x, INPUT);
   pinMode(y, INPUT);
-  pinMode(airSensor, INPUT);
-
-  yZero = analogRead(y);
-  xZero = analogRead(x);
+  pinMode(air_sensor, INPUT);
+  pinMode(joystick_sel_pin, INPUT_PULLUP);
+  delay(1000);
+  
+  Mouse.begin();
+  key.debounceTime = debounce_time;
+  key.multiclickTime = multi_click_time;
+  key.longClickTime = long_click_time;
+  
+  y_zero = analogRead(y);
+  x_zero = analogRead(x);
 }
 
 void loop() {
   static unsigned long lastLoop;
 
-  int yReading = analogRead(y);
-  int xReading = analogRead(x);
+  int x_reading = analogRead(x);
+  int y_reading = analogRead(y);
+  int air_reading = analogRead(air_sensor);
 
-  int yVal = (yReading - yZero) / sensitivity;
-  int xVal = (xReading - xZero) / sensitivity;
-  
-  if (debugCursor) {
-    Serial.print("yZero :");
-    Serial.println(yZero);
-    Serial.print("xZero :");
-    Serial.println(xZero);
-  
+  int y_val = (y_reading - y_zero) / sensitivity;
+  int x_val = (x_reading - x_zero) / sensitivity;
+
+  if (invert) {
+    x_val *= -1;
+    y_val *= -1;
+  }
+
+  if (debug_cursor) {
+    Serial.print("y_zero :");
+    Serial.println(y_zero);
+    Serial.print("x_zero :");
+    Serial.println(x_zero);
+
     Serial.print("y analog :");
-    Serial.println(yReading);
+    Serial.println(y_reading);
     Serial.print("x analog :");
-    Serial.println(xReading);
-  
+    Serial.println(x_reading);
+
     Serial.print("sensitivity :");
     Serial.println(sensitivity);
-    
+
     Serial.print("x value: ");
-    Serial.println(xVal);
+    Serial.println(x_val);
     Serial.print("y value: ");
-    Serial.println(yVal);
+    Serial.println(y_val);
   }
 
-  if (debugAirSensor) {
-    int reading = analogRead(airSensor);
-    Serial.println(reading);
+  if (debug_air_sensor) {
+    Serial.println(air_reading);
   }
 
+  // Move Mouse
+  if (!scroll_flag && !zoom_flag) {
+    if (y_val > 1 || y_val < -1) {
+      Mouse.move(0, y_val, 0);  // move mouse on y axis
+    }
+    if (x_val > 1 || x_val < -1) {
+      Mouse.move(x_val, 0, 0);  // move mouse on x axis
+    }
+    while (millis() - lastLoop < loop_period) {
+    // delay
+    }
+    lastLoop = millis(); 
+  }
 
-  if (yVal > 1 || yVal < -1) {
-      Mouse.move(0, yVal, 0);  // move mouse on y axis
+  key.Update();
+  int scroll_click_count = key.clicks;
+  if (scroll_click_count != 0) {
+    Serial.println(scroll_click_count);
+  }
+
+  // Scroll Mode
+  if ((scroll_click_count == -1 || scroll_flag) && key.depressed == true) {
+    scroll_flag = 1;
+    if (y_val <= -5) {
+      Mouse.move(0, 0, -1);
+      delay(100);
+    } else if (y_val >= 5) {
+      Mouse.move(0, 0, 1);
+      delay(100);
     }
-    if (xVal > 1 || xVal < -1) {
-      Mouse.move(xVal, 0, 0);  // move mouse on x axis
+  } else {
+    scroll_flag = 0;
+  }
+
+  // Zoom Mode
+  if ((scroll_click_count == -2 || zoom_flag) && key.depressed == true) {
+
+    zoom_flag = 1;
+    if (y_val <= -5) {
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press('='); 
+      Keyboard.releaseAll();
+      delay(500);
+    } else if ( y_val >= 5) {
+      Keyboard.press(KEY_LEFT_GUI);
+      Keyboard.press('-');
+      Keyboard.releaseAll();
+      delay(500);
     }
-    while(millis() - lastLoop < loopPeriod) {
-      // delay
-    }
-    lastLoop = millis();
+  } else {
+    zoom_flag = 0;
+  }
+  Serial.println(rest - air_reading);
+
+
+  // Detect Left and Right Clicks
+  if (air_reading - rest < -100 && !left_click_flag) {
+    left_click_flag = 1;
+    Mouse.press(MOUSE_LEFT);
+  } else if (air_reading - rest > -100 && air_reading - rest < 100 && left_click_flag) {
+    left_click_flag = 0;
+    Mouse.release(MOUSE_LEFT);
+  }
+  if (air_reading - rest > 100 && !right_click_flag) {
+    right_click_flag = 1;
+    Mouse.press(MOUSE_RIGHT);
+  } else if (air_reading - rest > -100 && air_reading - rest < 100 && right_click_flag) {
+    right_click_flag = 0;
+    Mouse.release(MOUSE_RIGHT);
+  }
+
+  // TODO: Detect Palette Command
 }
